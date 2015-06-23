e8c2e6aefbf0af1bea093b442d3cbb5a33f9220d
shun3475 <imokenpi3475@gmail.com>
2015-06-24 00:22:03 +0900

    Restaurantクラスの実装

--- GourmetApp/Restaurant.swift
+++ GourmetApp/Restaurant.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+public class Restaurant: Printable {
+    public var name: String? = nil
+    public var ruby: String? = nil
+    public var lat: Double? = nil
+    public var lon: Double? = nil
+    public var genre: String? = nil
+    public var date: NSDate? = nil
+    public var address: String? = nil
+    public var tel: String? = nil
+    public var remarks: String? = nil
+    public var country: String? = nil
+    public var city: String? = nil
+    public var state: String? = nil
+    public var cc: String? = nil
+    private static var api: API? = API.sharedInstance
+
+    public var description: String {
+        get {
+            var string = "\nName: \(name)\n"
+            return string
+        }
+    }
+    
+    public class func search(#lat: Double, lon: Double) -> [Restaurant]{
+        var restaurants: [Restaurant] = [Restaurant]()
+        api?.request(.GET, url: "restaurants/search", params: ["lon": 1.111, "lat": 1.111],
+            completion: {
+                (request, response, json, error) -> Void in
+                // TODO: 下記にjson取得終了時に行いたい処理を書く
+                println(json)
+                
+        })
+        return [Restaurant]()
+    }
+}
