1244340ac0f3b8d86160c73d1490ea0acc65b445
shun3475 <imokenpi3475@gmail.com>
2015-06-23 20:33:14 +0900

    install xcode project

---
+++ GourmetApp/GourmetApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GourmetApp
+//
+//  Created by 岩瀬　駿 on 2015/06/23.
+//  Copyright (c) 2015年 岩瀬　駿. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
