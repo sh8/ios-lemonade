3c25292e0b8b06f3f0c4daceff77d5994a33d162
shun3475 <imokenpi3475@gmail.com>
2015-06-26 01:04:55 +0900

    add MypageCollectionViewCell

---
+++ GourmetApp/MypageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MypageCollectionViewCell.swift
+//  GourmetApp
+//
+//  Created by 岩瀬　駿 on 2015/06/25.
+//  Copyright (c) 2015年 岩瀬　駿. All rights reserved.
+//
+
+import UIKit
+
+class MypageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photo: UIImageView!
+}
