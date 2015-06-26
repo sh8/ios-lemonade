6397abf0a8a33eed358f53b5bc2e211a7eb009d2
shun3475 <imokenpi3475@gmail.com>
2015-06-26 19:50:46 +0900

    マイページのヘッダーを管理するクラスを作成

---
+++ GourmetApp/MypageCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  MypageCollectionReusableView.swift
+//  GourmetApp
+//
+//  Created by 岩瀬　駿 on 2015/06/26.
+//  Copyright (c) 2015年 岩瀬　駿. All rights reserved.
+//
+
+import UIKit
+
+class MypageCollectionReusableView: UICollectionReusableView {
+        
+}
