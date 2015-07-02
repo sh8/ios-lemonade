db92a70b8f80cec1cd1c3804e2a8115f98fff035
shun3475 <imokenpi3475@gmail.com>
2015-07-03 00:09:09 +0900

    登録の処理の実装

--- GourmetApp/AuthenticateViewController.swift
+++ GourmetApp/AuthenticateViewController.swift
@@ -11,21 +11,25 @@ import Accounts
 
 class AuthenticateViewController: UIViewController {
 
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var screenNameField: UITextField!
     var accountStore: ACAccountStore = ACAccountStore()
+    var twitterAccount: ACAccount?
+    let defaults = NSUserDefaults.standardUserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func loginButtonTapped(sender: UIButton) {
-    performSegueWithIdentifier("PushToTabMenu", sender: nil)
+        signUp()
+        performSegueWithIdentifier("PushToTabMenu", sender: nil)
     }
 
     @IBAction func facebookButtonTapped(sender: UIButton) {
@@ -44,9 +48,9 @@ class AuthenticateViewController: UIViewController {
                 if facebookAccounts.count > 0 {
                     let facebookAccount: ACAccount? = facebookAccounts.last
                     let email: String = facebookAccount?.valueForKey("properties")?.objectForKey("ACUIDisplayUsername") as! String
+                    println(facebookAccount?.valueForKey("properties"))
                     let access_token: String? = facebookAccount?.credential.oauthToken
-                    println(email)
-                    println(access_token)
+                    
                 }
             }
             
@@ -65,11 +69,56 @@ class AuthenticateViewController: UIViewController {
             if granted {
                 let twitterAccounts: [ACAccount] = (self.accountStore.accountsWithAccountType(twitterAccountType) as? [ACAccount])!
                 if twitterAccounts.count > 0 {
+                    self.showAccountSelectSheet(twitterAccounts)
                     let twitterAccount: ACAccount? = twitterAccounts.last
                 }
             }
             
         })
+    }
+    
+    @IBAction func onTapped(sender: UITapGestureRecognizer) {
+        screenNameField.resignFirstResponder()
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+    }
+   
+    // アプリケーションロジック
+    func signUp() {
+        let name = screenNameField.text
+        let email = emailField.text
+        let password = passwordField.text
+        API.request(.POST, url: "users/sign_up", params: ["user": ["screen_name": name, "email": email, "password": password]], completion: {
+            (request, response, json, error) -> Void in
+            for (key, value) in json {
+                self.defaults.setObject(json["access_token"].string, forKey: "ACCESS_TOKEN")
+                self.defaults.synchronize()
+                println(self.defaults.objectForKey("ACCESS_TOKEN"))
+            }
+        })
+    }
+    
+    private func showAccountSelectSheet(accounts: [ACAccount]) {
+        
+        let alert = UIAlertController(title: "Twitter",
+            message: "アカウントを選択してください",
+            preferredStyle: .ActionSheet)
+        
+        // アカウント選択のActionSheetを表示するボタン
+        for account in accounts {
+            alert.addAction(UIAlertAction(title: account.username,
+                style: .Default,
+                handler: { (action) -> Void in
+                    println("your select account is \(account)")
+                    self.twitterAccount = account
+            }))
+        }
+        
+        // キャンセルボタン
+        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        // 表示する
+        self.presentViewController(alert, animated: true, completion: nil)
     }
     
     /*
