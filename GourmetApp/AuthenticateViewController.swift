4fd701c4aea514b87f79ba12b31ace6f2bb8e0e0
shun3475 <imokenpi3475@gmail.com>
2015-07-03 16:24:25 +0900

    ログイン処理を実装した

--- GourmetApp/AuthenticateViewController.swift
+++ GourmetApp/AuthenticateViewController.swift
@@ -15,11 +15,19 @@ class AuthenticateViewController: UIViewController {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var screenNameField: UITextField!
     var accountStore: ACAccountStore = ACAccountStore()
-    var twitterAccount: ACAccount?
+    var account: ACAccount!
     let defaults = NSUserDefaults.standardUserDefaults()
+    let TWITTER = 1
+    let FACEBOOK = 2
+    let nsnc = NSNotificationCenter.defaultCenter()
+    let LOGIN_COMPLETED = "LOGIN_COMPLETED"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        nsnc.addObserverForName(self.LOGIN_COMPLETED, object: nil, queue: nil, usingBlock: {
+            (notification) in
+            self.performSegueWithIdentifier("PushToTabMenu", sender: nil)
+        })
     }
     
     override func didReceiveMemoryWarning() {
@@ -29,7 +37,6 @@ class AuthenticateViewController: UIViewController {
     
     @IBAction func loginButtonTapped(sender: UIButton) {
         signUp()
-        performSegueWithIdentifier("PushToTabMenu", sender: nil)
     }
 
     @IBAction func facebookButtonTapped(sender: UIButton) {
@@ -47,13 +54,11 @@ class AuthenticateViewController: UIViewController {
                 let facebookAccounts: [ACAccount] = (self.accountStore.accountsWithAccountType(facebookAccountType) as? [ACAccount])!
                 if facebookAccounts.count > 0 {
                     let facebookAccount: ACAccount? = facebookAccounts.last
-                    let email: String = facebookAccount?.valueForKey("properties")?.objectForKey("ACUIDisplayUsername") as! String
-                    println(facebookAccount?.valueForKey("properties"))
                     let access_token: String? = facebookAccount?.credential.oauthToken
-                    
+                    self.account = facebookAccount
+                    self.signUp(sns_type: self.FACEBOOK)
                 }
             }
-            
         })
     }
     
@@ -84,15 +89,35 @@ class AuthenticateViewController: UIViewController {
     }
    
     // アプリケーションロジック
-    func signUp() {
-        let name = screenNameField.text
-        let email = emailField.text
-        let password = passwordField.text
-        API.request(.POST, url: "users/sign_up", params: ["user": ["screen_name": name, "email": email, "password": password]], completion: {
+    func signUp(sns_type: Int? = nil) {
+        var params = [String: AnyObject]()
+        if let sns = sns_type {
+            switch sns {
+            case self.TWITTER:
+                let name: String = self.account.username
+                let email: String = ""
+                let uid = self.account?.valueForKey("properties")?.objectForKey("user_id") as! String
+                params = ["user": ["screen_name": name, "email": email, "uid": uid, "sns_type": sns]]
+            case self.FACEBOOK:
+                let name = self.account.username
+                let email: String = self.account?.valueForKey("properties")?.objectForKey("ACUIDisplayUsername") as! String
+                let uid = self.account?.valueForKey("properties")?.objectForKey("uid") as! Int
+                params = ["user": ["screen_name": name, "email": email, "uid": uid, "sns_type": sns]]
+            default:
+                break
+            }
+        } else {
+            let name = screenNameField.text
+            let email = emailField.text
+            let password = passwordField.text
+            params = ["user": ["screen_name": name, "email": email, "password": password]]
+        }
+        API.request(.POST, url: "users/sign_up", params: params, completion: {
             (request, response, json, error) -> Void in
             for (key, value) in json {
                 self.defaults.setObject(json["access_token"].string, forKey: "ACCESS_TOKEN")
                 self.defaults.synchronize()
+                self.nsnc.postNotificationName(self.LOGIN_COMPLETED, object: nil)
                 println(self.defaults.objectForKey("ACCESS_TOKEN"))
             }
         })
@@ -109,8 +134,8 @@ class AuthenticateViewController: UIViewController {
             alert.addAction(UIAlertAction(title: account.username,
                 style: .Default,
                 handler: { (action) -> Void in
-                    println("your select account is \(account)")
-                    self.twitterAccount = account
+                    self.account = account
+                    self.signUp(sns_type: self.TWITTER)
             }))
         }
         
