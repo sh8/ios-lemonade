b2d8f5beb75b8106cb8a63c04135a7e2e945fcc7
shun3475 <imokenpi3475@gmail.com>
2015-07-05 17:20:06 +0900

    create TimeLineViewController

---
+++ GourmetApp/TimeLineViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TimeLineViewController.swift
+//  GourmetApp
+//
+//  Created by 岩瀬　駿 on 2015/07/05.
+//  Copyright (c) 2015年 岩瀬　駿. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
