e089649e0566960d89971cc612fa88463ba0ea2f
shun3475 <imokenpi3475@gmail.com>
2015-07-05 13:33:30 +0900

    create Custom Tab Bar Controller

---
+++ GourmetApp/CustomUITabBarController.swift
@@ -0,0 +1,49 @@
+//
+//  CustomUITabBarController.swift
+//  GourmetApp
+//
+//  Created by 岩瀬　駿 on 2015/07/05.
+//  Copyright (c) 2015年 岩瀬　駿. All rights reserved.
+//
+
+import UIKit
+
+class CustomUITabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        println(viewController.restorationIdentifier)
+        if viewController.restorationIdentifier == "PostViewNavigation" {
+            if let currentVC = self.selectedViewController {
+                //表示させるモーダル
+                viewController.view.backgroundColor = UIColor.whiteColor()
+                let modalViewController: PostViewController = self.storyboard?.instantiateViewControllerWithIdentifier("PostViewController") as! PostViewController
+                currentVC.presentViewController(modalViewController, animated: true, completion: {
+                    tabBarController.selectedIndex = 0
+                })
+            }
+        }
+        return true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
