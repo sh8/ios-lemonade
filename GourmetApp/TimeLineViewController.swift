0ee3c902295012a1158f7e368342a82c57c406fa
shun3475 <imokenpi3475@gmail.com>
2015-07-08 00:57:16 +0900

    InstagramUIを実装

--- GourmetApp/TimeLineViewController.swift
+++ GourmetApp/TimeLineViewController.swift
@@ -8,12 +8,32 @@
 
 import UIKit
 
-class TimeLineViewController: UIViewController {
+class TimeLineViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    let nsnc = NSNotificationCenter.defaultCenter()
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        nsnc.addObserverForName(API.APILoadCompleteNotification, object: nil, queue: nil, usingBlock: {
+            (notification) in
+            self.tableView.reloadData()
+        })
+        
+        if self.tableView.respondsToSelector("separatorInset") {
+            self.tableView.separatorInset = UIEdgeInsetsZero;
+        }
+        
+        if self.tableView.respondsToSelector("layoutMargins") {
+            self.tableView.layoutMargins = UIEdgeInsetsZero;
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        getTimeLine()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +41,58 @@ class TimeLineViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - UITableViewDelegate
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 600
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    // MARK: - UITableViewdDataSource
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            if indexPath.row < posts.count {
+                let cell = tableView.dequeueReusableCellWithIdentifier("TimeLine") as! TimeLineTableViewCell
+                // TODO: - セルにデータを追加する処理を書く
+                cell.screenName.text = self.posts[indexPath.row].user.name!
+                cell.restaurantName.text = self.posts[indexPath.row].restaurant.name!
+                cell.imageHeight.constant = self.view.frame.width
+                cell.photo.contentMode = UIViewContentMode.ScaleAspectFit
+                cell.photo.sd_setImageWithURL(NSURL(string: self.posts[indexPath.row].photoName!))
+                
+                if cell.respondsToSelector("separatorInset") {
+                    cell.separatorInset = UIEdgeInsetsZero;
+                }
+                if cell.respondsToSelector("preservesSuperviewLayoutMargins") {
+                    cell.preservesSuperviewLayoutMargins = false;
+                }
+                if cell.respondsToSelector("layoutMargins") {
+                    cell.layoutMargins = UIEdgeInsetsZero;
+                }
+                return cell
+            }
+        }
+        return UITableViewCell()
+    }
+    
+    // MARK: - アプリケーションロジック
+    func getTimeLine() {
+        API.request(.GET, url: "time_lines", params: nil, completion: {
+            (request, response, json, error) -> Void in
+            for (key, value) in json {
+                var post = Post()
+                post.photoName = value["photo"]["url"].string
+                post.user.id = value["user"]["name"].int
+                post.user.name = value["user"]["name"].string
+                post.restaurant.id = value["restaurant"]["id"].int
+                post.restaurant.name = value["restaurant"]["name"].string
+                self.posts.append(post)
+            }
+        })
+    }
+
 
     /*
     // MARK: - Navigation
