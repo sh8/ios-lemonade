8bb1c40f2da384aaad09f2bb54fc4620f01facc0
shun3475 <imokenpi3475@gmail.com>
2015-07-14 00:16:41 +0900

    create RestaurantDetailViewController

---
+++ GourmetApp/RestaurantDetailViewController.swift
@@ -0,0 +1,108 @@
+//  RestaurantDetailViewController.swift
+//  GourmetApp
+//
+//  Created by 岩瀬　駿 on 2015/07/13.
+//  Copyright (c) 2015年 岩瀬　駿. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class RestaurantDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var topLeftPhoto: UIImageView!
+    @IBOutlet weak var topRightPhoto: UIImageView!
+    @IBOutlet weak var topLeftPhotoConstraint: NSLayoutConstraint!
+    @IBOutlet weak var topRightPhotoConstraint: NSLayoutConstraint!
+    @IBOutlet weak var restaurantInfo: UIView!
+    @IBOutlet weak var restaurantName: UILabel!
+    @IBOutlet weak var genre: UILabel!
+    @IBOutlet weak var address: UILabel!
+    @IBOutlet weak var tel: UILabel!
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let nsnc = NSNotificationCenter.defaultCenter()
+    var firstPhoto: String? = nil
+    var secondPhoto: String? = nil
+    var restaurant = Restaurant()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.restaurantName.text = self.restaurant.name
+        self.genre.text = self.restaurant.genre
+        self.address.text = self.restaurant.address
+        self.tel.text = self.restaurant.tel
+        self.getRestaurantDetail()
+        nsnc.addObserverForName(API.APILoadCompleteNotification, object: nil, queue: nil, usingBlock:{
+            (notification) in
+            println(self.firstPhoto)
+            if let firstPhoto = self.firstPhoto {
+                self.topLeftPhoto.sd_setImageWithURL(NSURL(string: firstPhoto))
+            }
+            if let secondPhoto = self.secondPhoto{
+                self.topLeftPhoto.sd_setImageWithURL(NSURL(string: secondPhoto))
+            }
+            
+            self.tableView.reloadData()
+            }
+        )
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 500
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurant.posts.count
+    }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            if indexPath.row < restaurant.posts.count {
+                let cell = tableView.dequeueReusableCellWithIdentifier("RestaurantDetail") as! TimeLineTableViewCell
+                cell.profilePhoto.sd_setImageWithURL(NSURL(string: self.restaurant.posts[indexPath.row].user.profilePhoto!))
+                cell.photo.sd_setImageWithURL(NSURL(string: self.restaurant.posts[indexPath.row].photoName!))
+                cell.screenName.text = self.restaurant.posts[indexPath.row].user.name
+                return cell
+            }
+        }
+        return UITableViewCell()
+    }
+    
+    
+    // MARK: - アプリケーションロジック
+    func getRestaurantDetail() {
+        API.request(.GET, url: "restaurants/\(restaurant.id!)", params: nil, completion: {
+            (request, response, json, error) -> Void in
+            self.firstPhoto = json["firstPhoto"].string
+            self.secondPhoto = json["firstPhoto"].string
+            for (key, value) in json {
+                var post = Post()
+                post.photoName = value["photo"]["url"].string
+                post.user.name = value["user"]["name"].string
+                self.restaurant.posts.append(post)
+            }
+        })
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
