b9becafd48cc12e0da6efc14e4f801792c161781
shun3475 <imokenpi3475@gmail.com>
2015-07-14 00:12:23 +0900

    create RestaurantDetailTableViewCell

---
+++ GourmetApp/RestaurantDetailTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  RestaurantDetailTableViewCell.swift
+//  GourmetApp
+//
+//  Created by 岩瀬　駿 on 2015/07/13.
+//  Copyright (c) 2015年 岩瀬　駿. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profilePhoto: UIImageView!
+    @IBOutlet weak var screenName: UILabel!
+    @IBOutlet weak var photo: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
