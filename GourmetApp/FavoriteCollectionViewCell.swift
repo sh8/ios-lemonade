a694d4886d6cd18b6d5555dcbd9df83228d0b080
shun3475 <imokenpi3475@gmail.com>
2015-07-18 00:54:31 +0900

    create FavoriteCollectionViewCell

---
+++ GourmetApp/FavoriteCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteCollectionViewCell.swift
+//  GourmetApp
+//
+//  Created by 岩瀬　駿 on 2015/07/17.
+//  Copyright (c) 2015年 岩瀬　駿. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photo: UIImageView!
+}
