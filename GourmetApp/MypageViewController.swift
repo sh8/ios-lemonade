306cbd439297a02ecd9d179b6c42e674a51d0eef
shun3475 <imokenpi3475@gmail.com>
2015-07-20 23:09:43 +0900

    フォロー機能を実装

--- GourmetApp/MypageViewController.swift
+++ GourmetApp/MypageViewController.swift
@@ -15,6 +15,8 @@ class MypageViewController: UIViewController, UICollectionViewDelegate, UICollec
     var posts: [Post] = [Post]()
     var user: User = User()
     var postsNumber: String? = nil
+    var followNumber: String? = nil
+    var followedNumber: String? = nil
     
     let nsnc = NSNotificationCenter.defaultCenter()
 
@@ -23,7 +25,7 @@ class MypageViewController: UIViewController, UICollectionViewDelegate, UICollec
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
         
-        if self.navigationController?.restorationIdentifier == "FriendList" {
+        if self.navigationController?.restorationIdentifier != "Mypage" {
             friendListButton.enabled = false
             friendListButton.tintColor = UIColor.clearColor()
         }
@@ -54,9 +56,13 @@ class MypageViewController: UIViewController, UICollectionViewDelegate, UICollec
                     (request, response, json, error) -> Void in
                     self.user.screenoName = json["user"]["screen_name"].string
                     self.user.name = json["user"]["name"].string
+                    self.user.isCurrentUser = json["is_current_user"].bool
+                    self.user.isFollowUser = json["is_follow_user"].bool
                     self.user.email = json["user"]["email"].string
                     self.user.profilePhoto = json["user"]["profile_photo"]["url"].string
                     self.postsNumber = json["posts_number"].string
+                    self.followNumber = json["follow_number"].string
+                    self.followedNumber = json["followed_number"].string
                     for (key, value) in json["posts"] {
                         var post = Post()
                         post.photoName = value["photo"]["url"].string
@@ -71,9 +77,13 @@ class MypageViewController: UIViewController, UICollectionViewDelegate, UICollec
                     (request, response, json, error) -> Void in
                     self.user.screenoName = json["user"]["screen_name"].string
                     self.user.name = json["user"]["name"].string
+                    self.user.isCurrentUser = json["is_current_user"].bool
+                    self.user.isFollowUser = json["is_follow_user"].bool
                     self.user.email = json["user"]["email"].string
                     self.user.profilePhoto = json["user"]["profile_photo"]["url"].string
                     self.postsNumber = json["posts_number"].string
+                    self.followNumber = json["follow_number"].string
+                    self.followedNumber = json["followed_number"].string
                     for (key, value) in json["posts"] {
                         var post = Post()
                         post.photoName = value["photo"]["url"].string
@@ -109,8 +119,26 @@ class MypageViewController: UIViewController, UICollectionViewDelegate, UICollec
             let header = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: "UserInfoHeader", forIndexPath: indexPath) as! MypageCollectionReusableView
             header.screenName.text = self.user.screenoName
             header.postsNumber.text = self.postsNumber
+            header.followNumber.text = self.followNumber
+            header.followedNumber.text = self.followedNumber
             header.profilePhoto.layer.cornerRadius = 5.0
             header.profilePhoto.layer.masksToBounds = true
+            if let isCurrentUser = self.user.isCurrentUser {
+                if isCurrentUser {
+                    header.editProfileButton.setTitle("プロフィールを編集", forState: UIControlState.Normal)
+                    header.editProfileButton.addTarget(self, action: "editProfile:", forControlEvents: UIControlEvents.TouchUpInside)
+                } else {
+                    if let isFollowUser = self.user.isFollowUser {
+                        if isFollowUser {
+                            header.editProfileButton.setTitle("フォローを解除する", forState: UIControlState.Normal)
+                            header.editProfileButton.addTarget(self, action: "unfollowUser:", forControlEvents: UIControlEvents.TouchUpInside)
+                        }
+                    } else {
+                        header.editProfileButton.setTitle("フォローする", forState: UIControlState.Normal)
+                        header.editProfileButton.addTarget(self, action: "followUser:", forControlEvents: UIControlEvents.TouchUpInside)
+                    }
+                }
+            }
             if let profilePhoto = self.user.profilePhoto {
                 header.profilePhoto.sd_setImageWithURL(NSURL(string: profilePhoto))
             }
@@ -132,8 +160,25 @@ class MypageViewController: UIViewController, UICollectionViewDelegate, UICollec
         return UICollectionViewCell()
     }
     
+    // MARK: - IBAction
     @IBAction func friendListButtonTapped(sender: UIBarButtonItem) {
         performSegueWithIdentifier("PushToFriendList", sender: nil)
+    }
+    
+    @IBAction func editProfile(sernder: UIButton) {
+        performSegueWithIdentifier("PushToEditPage", sender: self.user)
+    }
+    
+    @IBAction func followUser(sernder: UIButton) {
+        API.request(.POST, url: "friend_ships/create", params: ["id": self.user.id!],
+            completion:{
+                (request, response, json, error) -> Void in
+            }
+        )
+    }
+    
+    @IBAction func unfollowUser(sernder: UIButton) {
+        // TODO: ユーザをフォローした時の処理を書く
     }
     
     /*
